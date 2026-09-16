d5fa073be76018e600b6ab0b936a87643ba9574d
EstefaniaMancipeM <estefaniamancipe@gmail.com>
2026-09-15 22:09:22 -0500

    feat: agregar aplicacion principal

---
+++ src/main/java/com/estefania/planificador/PlanificadorBackendApplication.java
@@ -0,0 +1,12 @@
+package com.estefania.planificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificadorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanificadorBackendApplication.class, args);
+    }
+}
